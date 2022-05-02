add6ccfd8494cd7abc37dac863c79364ae7fd0e9
Laurens Engwegen <laurensengwegen@gmail.com>
2022-05-02 16:31:53 +0200

    added train function for lstm

--- LSTM_Classifier.py
+++ LSTM_Classifier.py
@@ -18,10 +18,12 @@ class LSTMModel(nn.Module):
                             dropout=dropout_rate)
 
         self.linear = nn.Linear(n_hidden, n_classes)
+        self.softmax = F.softmax()
 
     def forward(self, x):
         _, (hidden_state, _) = self.lstm(x)
         output = self.linear(hidden_state[-1]) # Last layer, last state
+        output = self.softmax(output)
         return output
 
 class LSTM_Classifier():
@@ -29,12 +31,44 @@ class LSTM_Classifier():
                  n_hidden=256, 
                  n_layers=2, 
                  dropout_rate=0.5,
-                 loss=nn.CrossEntropyLoss,
-                 optimizer=optim.Adam)
+                 loss_fct=nn.CrossEntropyLoss,
+                 optimizer=optim.Adam):
         
         n_features = x.shape[0] # nr electrodes
         n_classes = len(np.unique(y)) # nr classes
 
         self.model = LSTMModel(n_features, n_classes, n_hidden, n_layers, dropout_rate)
-        self.loss = loss()
-        self.optimizer = optimizer(self.model.parameters())
+        self.loss_fct = loss_fct
+        self.optimizer = optimizer
+
+    def train(self, X_train, y_train, n_epochs=15, verbose=0):
+        self.model.train()
+        # Initialize optimizer and loss function
+        optimizer = self.optimizer(self.model.parameters())
+        lossfunction = self.loss_fct()
+        # Start training for n_epochs
+        for epoch in range(n_epochs):
+            if verbose:
+                print(f'\nEpoch {epoch+1}/{n_epochs}...')
+            epoch_loss = 0
+            for X, y in zip(X_train, y_train):
+                optimizer.zero_grad()
+                X = X[None, :] # Needed when unbatched
+                y = y[None, :]
+                # y = argmax eerst
+                output = self.forward(X)
+                # print(output)
+                output = output[None, :]
+                print('\n\n\n')
+                print(output)
+                print(y)
+                print(torch.argmax(output))
+                print(torch.argmax(y))
+                loss = lossfunction(output, y)
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss
+            epoch_loss = epoch_loss/X_train.shape[0]
+            if verbose:
+                print(f'Avg. loss = {epoch_loss}')
